186ba84f85804fb26371bd0550b1a96447d9b0f8
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-08-11 17:38:19 +0300

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Maxim Golovlev on 11.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
